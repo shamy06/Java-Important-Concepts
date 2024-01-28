ceb7d014b4856a1f68a25662f9c2a622952b806a
Shubham Yadav <41867232+shamy06@users.noreply.github.com>
2024-01-28 10:00:34 +0530

    java

--- Java_Concepts/Array/1dArray.java
+++
@@ -1,8 +0,0 @@
-class TestArray{
-    public static void main(String args[]){
-        int a[] = {33,4,56,46};
-        for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
-        }
-    }
-}
